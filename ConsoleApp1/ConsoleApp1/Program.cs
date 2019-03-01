fdda104abbaa52ff14e707744419f08d32dfb0b7
Chukka, Murali Krishna Sai (UMKC-Student) <mchukka@mail.umkc.edu>
2019-03-01 16:44:46 -0600

    inital script

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+
+            process.StandardInput.WriteLine(@"cd C:\Users\mcmc5\Desktop\D-Drive\sample");
+            process.StandardInput.WriteLine(@"git clone https://stash.umkc.edu/scm/~mcmc5/contactus.git");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            Console.ReadKey();
+            
+        }
+    }
+}
